357a6c418ab070a6366aba84cea7310f38d6c265
yd-Vielma <yonathanvielma22@gmail.com>
2024-06-26 15:00:10 -0400

    pa

--- run.py
+++ run.py
@@ -1,18 +1,25 @@
 #invocar a flask
-from flask import Flask
+from flask import Flask,render_template
 
 #crear una app mediante la instancia
 app=Flask(__name__)
 
 #crear la ruta con su correspondientes funciones 
 
+#Inicio
 @app.route('/',methods=['GET'])
 def hola_mundo():
-    return f"Hola mundo"
+    return render_template('index.html')
 
-@app.route('/ejecucion',methods=['GET'])  #indicamos el metodo GET
+#formulario
+@app.route('/formulario',methods=['GET'])  #indicamos el metodo GET
+def formulario():
+    return render_template('formulario.html')
+
+# mis proyectos
+@app.route('/mis_proyectos', methods=['GET'])
 def mis_proyectos():
-    return "Aqui se ejecuta el proyecto"
+    return render_template('proyectos.html')
 
 
 #Ejercutar nuestra app cuando ejecutemos este archivo run.py
