499fc5bb463ee3b30ffce2ab93964e58a799049a
yd-Vielma <yonathanvielma22@gmail.com>
2024-07-12 15:18:20 -0400

    actualizacion de templates

--- run.py
+++ run.py
@@ -1,21 +1,31 @@
 #invocar a flask
-from flask import Flask
+from flask import Flask,render_template
 
 #crear una app mediante la instancia
 app=Flask(__name__)
 
 #crear la ruta con su correspondientes funciones 
 
+#inicio
 @app.route('/',methods=['GET'])
-def hola_mundo():
-    return f"Hola mundo"
+def index():
+    return render_template('/index.html')
 
-@app.route('/ejecucion',methods=['GET'])  #indicamos el metodo GET
+#mis proyectos
+@app.route('/proyectos',methods=['GET'])  #indicamos el metodo GET
 def mis_proyectos():
     return "Aqui se ejecuta el proyecto"
 
+#formulario
+@app.route('/formulario', methods=['GET','POST'])
+def formulario_registro():
+    return render_template('formulario.html')
 
-#Ejercutar nuestra app cuando ejecutemos este archivo run.py
+#vias de contacto
+@app.route('/contactos.html',methods=['GET'])
+def contactos():
+    return render_template('contacto.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
