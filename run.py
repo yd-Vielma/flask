43a3b5976135e024e3bf4da139983ba9a8b7ab94
yd-Vielma <yonathanvielma22@gmail.com>
2024-07-13 00:41:30 -0400

    Hemos aprendido a gestionar Get y Post

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #invocar a flask
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 #crear una app mediante la instancia
 app=Flask(__name__)
@@ -7,9 +7,13 @@ app=Flask(__name__)
 #crear la ruta con su correspondientes funciones 
 
 #inicio
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('/index.html')
+    if request.method=='POST':
+        nombre=request.form['Nombre']
+        return render_template('/index.html', nombre=nombre)
+    else:
+        return render_template('/index.html')
 
 #mis proyectos
 @app.route('/proyectos',methods=['GET'])  #indicamos el metodo GET
